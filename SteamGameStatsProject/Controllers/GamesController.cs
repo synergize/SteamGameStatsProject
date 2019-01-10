77b8fb2bda5022775a9dc209dd454b8871202cc8
Von Trinks <err0r142@gmail.com>
2019-01-09 17:10:54 -0800

    Inital Commit.

---
+++ SteamGameStatsProject/Controllers/GamesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SteamGameStatsProject.Models;
+
+namespace SteamGameStatsProject.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games/Stats
+        public ActionResult Stats()
+        {
+            var stats = new Stats() {Name = "Rust"};
+            return View(stats);
+        }
+    }
+}
